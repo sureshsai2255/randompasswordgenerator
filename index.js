3f21b84338f059a542b05c6aaa58f3506e07ba56
sureshsai2255 <72707066+sureshsai2255@users.noreply.github.com>
2024-08-15 16:49:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?","/"];
+
+let showPassword  = document.getElementById("password")
+let passwordLength = 15;
+function generatePassword(){
+    let resultArray = [];
+    while(resultArray.length<passwordLength){
+        resultArray.push( characters[Math.floor(Math.random()*characters.length)])
+    }
+    let password = resultArray.join('');
+    showPassword.value = password
+}
+
+
+
+
